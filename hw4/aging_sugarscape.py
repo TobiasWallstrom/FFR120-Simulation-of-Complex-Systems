59b1f52cf02de1309abe6513ad422ebaed36b3a3
Tobias Wallstrom <tobias@wallstrommarna.se>
2022-12-06 11:44:08 +0100

    hw 4

---
+++ hw4/aging_sugarscape.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+
+import numpy as np
+import numpy.typing as npt
+from typing import NewType
+
+from matplotlib import pyplot as plt
+from tqdm import trange
+
+Agent = NewType('Agent', npt.NDArray[int])
+Grid = NewType('Grid', npt.NDArray[npt.NDArray[int]])
+
+
+def gini(x):
+    mad = np.abs(np.subtract.outer(x, x)).mean()
+    # Relative mean absolute difference
+    rmad = mad/np.mean(x)
+    # Gini coefficient
+    g = 0.5 * rmad
+    return g
+
+
+def place_sugar(grid: Grid, row: int, col: int, radius: float) -> None:
+    # no idea in checking values guaranteed outside the radius
+    # use min and max to avoid index out of bounds
+    for i in range(max(row - int(np.ceil(radius)), 0), min(grid.shape[0], row + int(np.ceil(radius)))):
+        for j in range(max(col - int(np.ceil(radius)), 0), min(grid.shape[1], col + int(np.ceil(radius)))):
+            if (row - i) ** 2 + (col - j) ** 2 <= radius ** 2:
+                grid[i, j] += 1
+
+
+def place_agents(agents: npt.NDArray[Agent], N: int, regenerate: bool = False) -> npt.NDArray[Agent]:
+    agent_grid = np.zeros((N, N))
+    if regenerate:
+        for agent in agents:
+            if not agent[6]:
+                continue
+            i, j = agent[0:2]
+            agent_grid[int(i), int(j)] = 1
+
+    for a in range(agents.shape[0]):
+        free_spaces = np.argwhere(agent_grid == 0)
+        i, j = free_spaces[np.random.randint(len(free_spaces))]
+
+        agents[a, 0:2] = (i, j)
+        agent_grid[i, j] = 1
+    return agent_grid
+
+
+def generate_agents(num_agents: int) -> npt.NDArray[Agent]:
+    # One agent: [ pos_x, pos_y, wealth, vision, metabolism, alive, age, max_age ]
+    agents = np.zeros((num_agents, 8))
+
+    agents[:, 2] = np.random.randint(5, 26, num_agents)
+    agents[:, 3] = np.random.randint(1, 7, num_agents)
+    agents[:, 4] = np.random.randint(1, 5, num_agents)
+    agents[:, 5] = np.ones(num_agents)
+    agents[:, 6] = np.zeros(num_agents)
+    agents[:, 7] = np.random.randint(60, 100, num_agents)
+
+    return agents
+
+
+def regenerate_agent(agents: npt.NDArray[Agent], N: int) -> None:
+    reborn_agents = np.argwhere(agents[:, 5] == 0).flatten()
+
+    for agent in reborn_agents:
+        agents[agent, 2] = np.random.randint(5, 26)
+        agents[agent, 3] = np.random.randint(1, 7)
+        agents[agent, 4] = np.random.randint(1, 5)
+        agents[agent, 5] = 1
+        agents[agent, 6] = 0
+        agents[agent, 7] = np.random.randint(60, 100)
+
+
+def main():
+    num_agents = 400
+    N = 50
+    num_rounds = 500
+    regrowth_constant = 1
+    rng = np.random.default_rng()
+
+    sugar_grid = np.zeros((N, N))
+
+    for radius in [20, 15, 10, 5]:
+        place_sugar(sugar_grid, 40, 10, radius)
+        place_sugar(sugar_grid, 10, 40, radius)
+
+    max_capacity = sugar_grid.copy()
+    agents = generate_agents(num_agents)
+
+    agent_grid = place_agents(agents, N)
+
+    plt.figure(1)
+    plt.subplot(1, 2, 1)
+    plt.imshow(sugar_grid.copy(), cmap='summer')
+    plt.plot(agents[:, 0], agents[:, 1], 'r.')
+    plt.title('$t=0$')
+
+    plt.figure(2)
+    v_hist, v_bins = np.histogram(agents[:, 3], bins=[1, 2, 3, 4, 5, 6, 7])
+    m_hist, m_bins = np.histogram(agents[:, 4], bins=[1, 2, 3, 4, 5])
+    plt.subplot(1, 2, 1)
+    plt.bar(v_bins[:-1] - 0.2, v_hist, width=0.4, label='Initial')
+    plt.xlabel('Vision')
+    plt.ylabel('Number of agents')
+    plt.subplot(1, 2, 2)
+    plt.bar(m_bins[:-1] - 0.2, m_hist, width=0.4, label='Initial')
+    plt.xlabel('Metabolism')
+
+    wealth_hists = []
+    wealth_bins = []
+    timestamps = []
+
+    print(f'Initial Gini coefficient = {gini(agents[:,2])}')
+    plt.figure(4)
+    plt.suptitle('Lorenz plot')
+    plt.subplot(1, 2, 1)
+    plt.title('Initial')
+    wealth_lorenz = np.cumsum(np.sort(agents[:, 2]))/np.sum(agents[:, 2])
+    plt.plot([0, 1], [0, 1], color='k')
+    plt.plot(np.arange(num_agents)/num_agents, wealth_lorenz)
+
+    max_wealth = 0
+    for t in trange(num_rounds):
+        # Update agents
+        agent_order = rng.permuted(np.arange(agents.shape[0]))
+
+        if t % 20 == 0 and t < 80:  # get t = 20, t = 40, t = 60, and t = 80
+            alive_agents = agents[agents[:, 5] > 0]
+            s_hist, s_bins = np.histogram(alive_agents[:, 2], bins=15)
+            wealth_hists.append(s_hist)
+            wealth_bins.append(s_bins)
+            timestamps.append(f'${t = }$')
+
+        for i in agent_order:
+            if not agents[i, 5]:
+                continue
+
+            j = int(agents[i, 0])
+            k = int(agents[i, 1])
+            agents[i, 2] += (sugar_grid[j, k] - agents[i, 4]) * agents[i, 5]
+            # Kill if out of sugar
+            agents[i, 5] = (agents[i, 2] > 0).astype(int)
+            sugar_grid[j, k] = 0
+
+            most_sugar_found = 0
+
+            possible_destinations = []
+
+            vision = int(agents[i, 3])
+
+            # Right
+            for v in range(1, vision+1):
+                if j + v >= N:  # avoid index oob
+                    break
+                if agent_grid[j + v, k]:  # check next if spot is taken
+                    continue
+                sugar_found = sugar_grid[j + v, k]
+                if sugar_found >= most_sugar_found:  # add to possible destination if at least equal
+                    if sugar_found > most_sugar_found:  # clear list first if strictly more
+                        possible_destinations.clear()
+                    possible_destinations.append((j + v, k))
+                    most_sugar_found = sugar_found
+
+            # Left
+            for v in range(1, vision+1):
+                if j - v < 0:
+                    break
+                if agent_grid[j - v, k]:
+                    continue
+                sugar_found = sugar_grid[j - v, k]
+                if sugar_found >= most_sugar_found:
+                    if sugar_found > most_sugar_found:
+                        possible_destinations.clear()
+                    possible_destinations.append((j - v, k))
+                    most_sugar_found = sugar_found
+
+            # Up
+            for v in range(1, vision+1):
+                if k - v < 0:
+                    break
+                if agent_grid[j, k - v]:
+                    continue
+                sugar_found = sugar_grid[j, k - v]
+                if sugar_found >= most_sugar_found:
+                    if sugar_found > most_sugar_found:
+                        possible_destinations.clear()
+                    possible_destinations.append((j, k - v))
+                    most_sugar_found = sugar_grid[j, k - v]
+
+            # Down
+            for v in range(1, vision+1):
+                if k + v >= N:
+                    break
+                if agent_grid[j, k + v]:
+                    continue
+                sugar_found = sugar_grid[j, k + v]
+                if sugar_found >= most_sugar_found:
+                    if sugar_found > most_sugar_found:
+                        possible_destinations.clear()
+                    possible_destinations.append((j, k + v))
+                    most_sugar_found = sugar_grid[j, k + v]
+
+            j_max, k_max = rng.choice(possible_destinations) if len(possible_destinations) else (j, k)
+
+            agent_grid[j, k] = 0
+            agent_grid[j_max, k_max] = 1
+            agents[i, 0:2] = j_max, k_max
+
+        # Update sugar scape
+        sugar_grid += regrowth_constant
+        sugar_grid = np.minimum(sugar_grid, max_capacity)
+        # Age agent and kill if too old
+        agents[:, 6] += 1
+        agents[agents[:, 6] >= agents[:, 7], 5] = 0
+        if t == num_rounds - 1:  # regenerate unless final generation
+            break
+        regenerate_agent(agents, N)
+        place_agents(agents, N, True)
+        max_wealth = np.max(agents[:, 2]) if np.max(agents[:, 2]) > max_wealth else max_wealth
+
+    alive_agents = agents[agents[:, 5] > 0]
+    alive_agents = alive_agents[alive_agents[:, 6] < alive_agents[:, 7]]
+
+    print(f'Number of alive agents: {alive_agents.shape[0]}')
+
+    plt.figure(1)
+    plt.suptitle('Agent location, aging')
+    plt.subplot(1, 2, 2)
+    plt.imshow(sugar_grid, cmap='summer')
+    plt.plot(alive_agents[:, 0], alive_agents[:, 1], 'r.')
+    plt.title(f'$t={num_rounds}$')
+
+    plt.figure(2)
+    plt.suptitle('Visual ranges vs metabolism rates, aging')
+    v_hist, v_bins = np.histogram(alive_agents[:, 3], bins=[1, 2, 3, 4, 5, 6, 7])
+    m_hist, m_bins = np.histogram(alive_agents[:, 4], bins=[1, 2, 3, 4, 5])
+    plt.subplot(1, 2, 1)
+    plt.bar(v_bins[:-1] + 0.2, v_hist, width=0.4, label='Final')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.bar(m_bins[:-1] + 0.2, m_hist, width=0.4, label='Final')
+    plt.legend()
+
+    plt.figure(3)
+    plt.suptitle('Wealth distribution, aging')
+    wealth_index = wealth_bins[-1:].index(max(wealth_bins[-1:]))
+    s_bins = wealth_bins[wealth_index]
+    plt.hist(wealth_hists, s_bins, label=timestamps, alpha=0.8, density=True, histtype='bar')
+    plt.xlabel('Wealth')
+    plt.ylabel('Number of agents')
+    plt.legend()
+
+    print(f'Final Gini coefficient = {gini(agents[:,2])}')
+    plt.figure(4)
+    plt.suptitle('Gini coefficient, aging')
+    plt.subplot(1, 2, 2)
+    plt.title('Final')
+    wealth_lorenz = np.cumsum(np.sort(agents[:, 2]))/np.sum(agents[:, 2])
+    plt.plot([0, 1], [0, 1], color='k')
+    plt.plot(np.arange(agents.shape[0])/agents.shape[0], wealth_lorenz)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ hw4/schelling_anti_gregarious_segregation.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import numpy as np
+import numpy.typing as npt
+from scipy.signal import fftconvolve
+from tqdm import trange
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+# Schelling segregation model and anti-gregarious segregation model. To change between them, change mode in main().
+
+SCHELLING = 1
+ANTI_GREGARIOUS = -1
+
+
+def get_majority(town: npt.NDArray[npt.NDArray[int]]) -> npt.NDArray[npt.NDArray[int]]:
+    size = 3
+    convolution_kernel = np.ones((size, size)) / (size ** 2)
+    convolution = fftconvolve(town, convolution_kernel, mode='same')
+    return (convolution > 0).astype(int) - (convolution < 0).astype(int)
+
+
+def calculate_happiness(town: npt.NDArray[npt.NDArray[int]], mode: int = SCHELLING) -> (float, float, float):
+    happiness = get_majority(town) * town * mode
+
+    A = np.count_nonzero((happiness > 0) & (town > 0)) / np.count_nonzero(town > 0)
+    B = np.count_nonzero((happiness > 0) & (town < 0)) / np.count_nonzero(town < 0)
+    total = (A + B) / 2
+
+    return A, B, total
+
+
+def main():
+    mode = SCHELLING
+    N = 50
+    f = 0.1  # free houses
+    population_equal = (1 - f) / 2  # equal distribution between A and B in the occupied housing
+
+    rng = np.random.default_rng()
+    town = rng.permuted(np.concatenate((np.zeros(int(N ** 2 * f)), np.ones(int(N ** 2 * population_equal)), -
+                                        np.ones(int(N ** 2 * population_equal))))).reshape((N, N))
+
+    num_rounds = 100_000
+    plot_rounds = list(filter(lambda x: x <= num_rounds, [0, 10_000, 100_000]))
+    town_at_time = np.zeros((len(plot_rounds), N, N))
+    town_at_time[0] = town  # since we check if t+1 is in plot range, we need to manually add first town
+
+    happiness_a = np.zeros(num_rounds)
+    happiness_b = np.zeros(num_rounds)
+    avg_happiness = np.zeros(num_rounds)
+
+    move_happened = np.zeros(num_rounds, dtype=bool)
+
+    for t in trange(num_rounds):
+        happiness_a[t], happiness_b[t], avg_happiness[t] = calculate_happiness(town, mode)
+
+        happiness = get_majority(town) * town * mode  # mode inverts if anti-gregarious
+
+        occupied = np.argwhere(town != 0)
+        i, j = occupied[np.random.randint(len(occupied))]
+
+        if happiness[i, j] == -1:
+            move_happened[t] = True
+
+            free = np.argwhere(town == 0)
+            i_new, j_new = free[np.random.randint(len(free))]
+
+            town[i_new, j_new] = town[i, j]
+            town[i, j] = 0
+
+        if t+1 in plot_rounds:
+            town_at_time[plot_rounds.index(t+1)] = town
+
+    plotlen = len(town_at_time)
+    colors = [(230/256, 108/256, 0), (1, 1, 1), (241/256, 211/256, 19/256)]
+
+    cmap = LinearSegmentedColormap.from_list('', colors, N=3)
+    plt.suptitle('Schelling' if mode == SCHELLING else 'Anti-gregarious')
+
+    for i in range(plotlen):
+        plt.subplot(1, plotlen, i + 1)
+        plt.imshow(town_at_time[i], cmap=cmap)
+        plt.title(f'$t={plot_rounds[i]}$')
+
+    plt.figure()
+    rge = np.arange(num_rounds)
+    plt.suptitle('Schelling' if mode == SCHELLING else 'Anti-gregarious')
+
+    plt.plot(rge, happiness_a, label='Happiness (A)')
+    plt.plot(rge, happiness_b, label='Happiness (B)')
+    plt.plot(rge, avg_happiness, label='Happiness (total)')
+    window_size = 1000
+    plt.plot(rge[window_size:-window_size], (fftconvolve(move_happened, np.ones(window_size), mode='same') /
+             window_size)[window_size:-window_size], label=f'Moves (moving average, window={window_size})')
+    plt.legend()
+    plt.xlabel('$t$')
+    plt.ylabel('Happiness')
+    # plt.ylim((0, 1))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
